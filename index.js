ab3958a017430997ca680996c14ac20eb4fa3d8f
Konrad Głowacki <konrad.glowacki@wpengine.com>
2020-11-10 16:45:57 +0100

    Clear code-challenge folder

--- index.js
+++ index.js
@@ -2,34 +2,12 @@ const express = require('express');
 const server = express();
 const port = process.env.PORT || 4000;
 
-const fs = require('fs');
-const path = require('path');
-const merge = require('./code-challenge/merge');
-
 server.get('/', (req, res) => {
-  // This breaks logs via cli
-  console.log(process.env);
   res.send(`Hello World on port: ${port}`);
 });
 
 server.get('/envs', (req, res) => {
   res.send(process.env);
-});
-
-server.get('/code-challenge', async (req, res) => {
-  try {
-    const outputFile = path.resolve(process.cwd(), './code-challenge/output.csv');
-
-    await merge({
-      outputFile,
-      inputFile: path.resolve(process.cwd(), './code-challenge/input.csv'),
-    });
-
-    const fileContent = fs.readFileSync(outputFile).toString();
-    res.send(fileContent);
-  } catch (err) {
-    res.status(500).send(`Error: ${err}`);
-  }
 });
 
 server.listen(port, () => {
